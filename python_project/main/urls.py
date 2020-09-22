fe14fe615f2a944c754d9664615a34e5e31af7d7
nelsonhwu <69525077+nelsonhwu@users.noreply.github.com>
2020-09-22 16:02:56 -0700

    09/22/20 Class & Calendar
    Created Class & Calendar HTML.  Created a backend and a quick mock-up for front-end.  Will need backend finished to test out code.

--- python_project/main/urls.py
+++ python_project/main/urls.py
@@ -5,4 +5,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index),
+    path('add_project/<int:class_id>', views.add_project),
+    path('add_student/<int:class_id>', views.add_student),
+    path('class/<int:class_id>', views.class_render),
+    path('request_appointment', views.calendar),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- python_project/main/views.py
+++ python_project/main/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import User, Class, Project, Message, Comment
 
 def index(request):
     return HttpResponse("Python Project Basics Are Functioning")
 
+def add_project(request, class_id):
+    current_class = Class.objects.get(id=class_id)
+    title = request.POST['title']
+    due_date = request.POST['due_date']
+    desc = request.POST['desc']
+    new_project = Project.objects.create(
+        title = title, 
+        due_date = due_date, 
+        desc = desc
+    )
+    current_class.project_classes.add(new_project)
+    return redirect(f'/class/{class_id}')
 
+def add_student(request, class_id):
+    current_class = Class.objects.get(id=class_id)
+    new_student_id = request.POST['student.id']
+    new_student = User.objects.get(id=new_student_id)
+    current_class.parent_classes.add(new_student)
+    return redirect(f'/class/{class_id}')
+
+def class_render(request, class_id):
+    logged_in_user = User.obejcts.get(id=request.session['user_id'])
+    context={
+        this_class : Class.objects.get(id=class_id),
+        user : logged_in_user
+    }
+    return render(request, 'class.html', context)
+
+def calendar(request):
+    return render(request, 'calendar.html')
