e983955907b12627a5a6fab77441eed1e605de44
nelsonhwu <69525077+nelsonhwu@users.noreply.github.com>
2020-09-23 11:02:53 -0700

    09/23/20 AM
    Combining files

--- python_project/main/urls.py
+++ python_project/main/urls.py
@@ -9,4 +9,14 @@ urlpatterns = [
     path('add_student/<int:class_id>', views.add_student),
     path('class/<int:class_id>', views.class_render),
     path('request_appointment', views.calendar),
+    path('register', views.register),
+    path('add_user', views.add_user),
+    path('login', views.login),
+    path('log_in', views.log_in),
+    path('success', views.success),
+    path('logout', views.logout),
+    path('user_info',views.user),
+    path('edit_class/<int:class_id>', views.edit_class),
+
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- python_project/main/views.py
+++ python_project/main/views.py
@@ -32,5 +32,83 @@ def class_render(request, class_id):
     }
     return render(request, 'class.html', context)
 
+def edit_class(request, class_id):
+    context={
+        'class' : Class.objects.get(id=class_id)
+    }
+    return render(request, 'edit_class.html', context)
+
+def edit_class_post(request, class_id):
+    errors = User.objects.class_validation(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.errors(request.value)
+        return redirect(f'/edit_class/{class_id}')
+
+
 def calendar(request):
     return render(request, 'calendar.html')
+
+def register(request):
+    return render(request, "register.html")
+
+def add_user(request):
+    errors = User.objects.registration_validator(request.POST)
+    if len(errors) > 0:
+        for msg in errors.values():
+            messages.error(request, msg)
+        return redirect('/register')
+    password = request.POST['password']
+    pw_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
+    new_user = User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        password = pw_hash,
+        street_address = request.POST['street_address'],
+        city = request.POST['city'],
+        state = request.POST['state'],
+        zip_code = request.POST['zip_code'],
+        phone = request.POST['phone'],
+        access_level = request.POST['access_level']
+    )
+    request.session['user_id'] = new_user.id
+    return redirect('/success')
+
+def login(request):
+    return render(request, "login.html")
+
+def log_in(request):
+    errors = User.objects.login_validation(request.POST)
+    if len(errors) > 0:
+        for msg in errors.values():
+            messages.error(request, msg)
+        return redirect('/login')
+    list_of_users = User.objects.filter(email=request.POST['email'])
+    if len(list_of_users) > 0:
+        user = list_of_users[0]
+        if bcrypt.checkpw(request.POST['password'].encode('utf-8'), user.password.encode()):
+            request.session['user_id'] = user.id
+            return redirect('/success')
+        return redirect('/')
+
+def success(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    logged_in_user = User.objects.get(id=request.session['user_id'])
+    print(logged_in_user.__dict__)
+    context = {
+        'logged_in_user' : logged_in_user,
+    }
+    return render(request, "success.html", context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def user(request):
+    logged_in_user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'logged_in_user':logged_in_user,
+    }
+    return render(request, 'user_info.html',context)
