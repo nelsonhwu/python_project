656cee5e0a165441068148a23d85e01f265c85be
xvang3 <69734222+xvang3@users.noreply.github.com>
2020-09-22 18:04:33 -0800

    09/22/2020
    implemented login and registration after modification:
    error with related_user, would not allow creation of user if there was no instance of another user despite null: (i.e. infinite loop)
    took out regex on address because it checked a full address, but the form input all separate pieces as data, instead of one per the model,
    Don't know if it was just mine, but User model had no state, so I had to input it along with migrations,
    there may be other things, but I cannot remember

--- python_project/main/urls.py
+++ python_project/main/urls.py
@@ -5,6 +5,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index),
-    path('login', views.login),
     path('register', views.register),
+    path('add_user', views.add_user),
+    path('login', views.login),
+    path('log_in', views.log_in),
+    path('success', views.success),
+    path('logout', views.logout),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- python_project/main/views.py
+++ python_project/main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import User, Class, Project, Message, Comment, Image
+from django.contrib import messages
+import bcrypt
 
 def index(request):
     return render(request, "homepage.html")
@@ -6,6 +9,56 @@ def index(request):
 def register(request):
     return render(request, "register.html")
 
+def add_user(request):
+    errors = User.objects.registration_validator(request.POST)
+    if len(errors) > 0:
+        for msg in errors.values():
+            messages.error(request, msg)
+        return redirect('/register')
+    password = request.POST['password']
+    pw_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
+    new_user = User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        password = pw_hash,
+        street_address = request.POST['street_address'],
+        city = request.POST['city'],
+        state = request.POST['state'],
+        zip_code = request.POST['zip_code'],
+        phone = request.POST['phone'],
+        access_level = request.POST['access_level']
+    )
+    request.session['user_id'] = new_user.id
+    return redirect('/success')
+
 def login(request):
     return render(request, "login.html")
 
+def log_in(request):
+    errors = User.objects.login_validation(request.POST)
+    if len(errors) > 0:
+        for msg in errors.values():
+            messages.error(request, msg)
+        return redirect('/login')
+    list_of_users = User.objects.filter(email=request.POST['email'])
+    if len(list_of_users) > 0:
+        user = list_of_users[0]
+        if bcrypt.checkpw(request.POST['password'].encode('utf-8'), user.password.encode()):
+            request.session['user_id'] = user.id
+            return redirect('/success')
+        return redirect('/')
+
+def success(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    logged_in_user = User.objects.get(id=request.session['user_id'])
+    print(logged_in_user.__dict__)
+    context = {
+        'logged_in_user' : logged_in_user,
+    }
+    return render(request, "success.html", context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
