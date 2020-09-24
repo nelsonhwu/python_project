674507432e016f47fd021d16b6cfa6ea748ca1a3
xvang3 <69734222+xvang3@users.noreply.github.com>
2020-09-24 00:48:30 -0800

    09/24/2020
    Errors update

--- python_project/main/models.py
+++ python_project/main/models.py
@@ -48,7 +48,6 @@ class UserManager(models.Manager):
             errors['phone'] = 'Please enter your phone number with the area code.'
         if len(post_data['phone']) > 0 and not PHONE_REGEX.match(post_data['phone']):
             errors['invalid_phone'] = 'Please enter valid phone number.'   
-            errors['invalid_phone'] = 'Please enter valid phone number.'
         return errors
 
     def login_validator(self, post_data):
@@ -66,14 +65,12 @@ class UserManager(models.Manager):
             errors['class'] = 'Please include a class.'
         #if post_data['due_date'] < date.today().strftime("%Y-%m-%d"):
         #    error['due_date'] = "Deadline cannot be in the past"
-        if post_data['due_date'] < date.today().strftime("%Y-%m-%d"):
-            errors['due_date'] = "Deadline cannot be in the past"
         return errors
     
     def class_validator(self, post_data):
         errors={}
-        if len(post_data['subject']) < 2:
-            errors['subject'] = 'Subject needs at least 2 characters.'
+        if len(post_data['Subject']) < 2:
+            errors['Subject'] = 'Subject needs at least 2 characters.'
         if not post_data['schedule_day']:
             errors['schedule_day'] = 'Please include a day for class to be held.'
         if not post_data['schedule_time']:
@@ -87,77 +84,9 @@ class UserManager(models.Manager):
                 errors['email'] = "You cannot enter your own email"
         return errors
 
-    def update_class_validation(self, post_data):
-        errors={}
-        if post_data['subject'] and len(post_data['subject']) < 2:
-            errors['subject'] = 'Subject needs at least 2 characters.'
-        return errors
-
-    def edit_account_validation(self, post_data):
-        NAME_REGEX = re.compile(r'^[a-zA-Z]')
-        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-        PHONE_REGEX = re.compile(r'^\+?1?\d{9,15}$')
-        errors={}
-        if len(post_data['first_name']) != 0 and len(post_data['first_name']) < 2:
-            errors['first_name'] = 'First name needs at least 2 characters.'
-        if len(post_data['first_name']) != 0 and not NAME_REGEX.match(post_data['first_name']):
-            errors['first_name_char'] = 'First name should contain only letters'
-        
-        if len(post_data['last_name']) != 0 and len(post_data['last_name']) < 2:
-            errors['last_name'] = 'Last name needs at least 2 characters.'
-        if len(post_data['last_name']) != 0 and not NAME_REGEX.match(post_data['last_name']):
-            errors['last_name_char'] = 'Last name should contain only letters'
-        
-        if len(post_data['email']) != 0 and not EMAIL_REGEX.match(post_data['email']):
-            errors['invalid_email'] = 'Invalid email! Try Again!'
-        
-        if len(post_data['password']) != 0 and len(post_data['password']) < 8:
-            errors['password_length'] = 'Passwords need at least 8 characters.'
-        if post_data['password'] != post_data['confirm_password']:
-            errors['invalid_password'] = 'Password does not match with Confirm Password.'
-        
-        if len(post_data['street_address']) != 0 and len(post_data['street_address']) < 2:
-            errors['street_address'] = 'Your address needs at least 2 characters.'
-        #if len(post_data['street_address']) != 0 and not ADDRESS_REGEX.match(post_data['street_address']):
-        #    errors['invalid_street_address'] =  'Please enter a valid Address.'
-        
-        if len(post_data['city']) != 0 and len(post_data['city']) < 2:
-            errors['city'] = "Please enter your city address."
-
-        if len(post_data['state']) != 0 and len(post_data['state']) < 2:
-            errors['state'] = "Please enter your state."
-
-        if len(post_data['zip_code']) != 0 and len(post_data['zip_code']) < 4:
-            errors['zip_code'] = "Please enter your 5 digit zip code."
-        
-        if len(post_data['phone']) != 0 and len(post_data['phone']) < 10:
-            errors['phone'] = 'Please enter your phone number with the area code.'
-        if len(post_data['phone']) != 0 and not PHONE_REGEX.match(post_data['phone']):
-            errors['invalid_phone'] = 'Please enter valid phone number.'
-        # if len(post_data["related_user"]) != 0 and User.objects.filter(id=post_data["related_user"]).exists() == False:
-        #     errors["related_user"] = "The parent's you put in does not exsit!"
-        return errors
-
-    def edit_project_validation(self,post_data):
-        errors={}
-        if len(post_data['title']) != 0 and len(post_data['title']) < 2:
-            errors['title'] = 'Title needs at least 2 characters.'
-        if len(post_data['due_date']) !=0 and post_data['due_date'] < date.today().strftime("%Y-%m-%d"):
-            errors['due_date'] = "Deadline cannot be in the past."
-        if len(post_data['desc']) != 0 and len(post_data['desc']) < 2:
-            errors['desc'] = 'Description needs at least 2 characters.'
-
-        return errors
-
-    def upload_image_validation(self, post_data):
-        errors ={}
-        if len(post_data['title']) < 2:
-            errors['title'] = 'Title needs at least 2 characters.'
-        return errors
-
 class User(models.Model):
-    first_name = models.CharField(max_length = 255)
-    last_name = models.CharField(max_length = 255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     street_address = models.CharField(max_length = 255, blank=True, null=True)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
@@ -166,7 +95,6 @@ class User(models.Model):
     phone = models.IntegerField(blank=True, null=True)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length = 255)
-    access_level = models.CharField(max_length = 255)
     profile_image = models.ImageField(upload_to='profile_image', blank=True, null=True, default="profile1.png")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -197,6 +125,8 @@ class Relationship(models.Model):
     
     
 class Class(models.Model):
+    # teacher = models.ManyToManyField(Teacher, related_name="teacher_classes")
+    # student = models.ManyToManyField(Student, related_name="student_classes")
     user = models.ManyToManyField('User', related_name="parent_classes")
     subject = models.CharField(max_length=255)
     start_date = models.DateTimeField()
@@ -223,6 +153,9 @@ class Project(models.Model):
 class Message(models.Model):
     message = models.TextField()
     user = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
+    # parent_id = models.ForeignKey(Teacher, related_name="parent_messages", on_delete=models.CASCADE)
+    # student_id = models.ForeignKey(Teacher, related_name="student_messages", on_delete=models.CASCADE)
+    # access_level = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
@@ -233,6 +166,8 @@ class Comment(models.Model):
     comment = models.TextField()
     message = models.ForeignKey(Message, related_name="comments", on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
+    # parent_id = models.ForeignKey(Parent, related_name="parent_comments", on_delete=models.CASCADE)
+    # student_id = models.ForeignKey(Student, related_name="student_comments", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
---
+++ python_project1/main/urls.py
@@ -0,0 +1,55 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+####################################################################
+
+    path('class/<int:class_id>/edit', views.edit_class),
+    path('class/new', views.new_class),
+    path('class/create', views.create_class),
+    path('edit_class/<int:class_id>', views.edit_class_post),
+    path('image/all', views.image_block),
+    path('image/<int:image_id>', views.image_viewer),
+    path('image/<int:image_id>/add_message', views.add_message),
+    path('image/<int:image_id>/add_comment', views.add_comment),
+    path('image/<int:image_id>/delete/<int:message_id>', views.delete_message),
+
+
+####################################################################
+
+    path('', views.index),
+    path('add_project/<int:class_id>', views.add_project),
+    path('add_student/<int:class_id>', views.add_student),
+    path('class/<int:class_id>', views.class_render),
+    path('request_appointment', views.calendar),
+    path('register', views.register),
+    path('add_user', views.add_user),
+    path('login', views.login),
+    path('log_in', views.log_in),
+    path('success', views.success),
+    path('logout', views.logout),
+    path('user_info',views.user)
+
+    path('class/<int:class_id>/edit', views.edit_class),
+    path('class/new', views.new_class),
+    path('class/create', views.create_class),
+    path('edit_class/<int:class_id>', views.edit_class_post),
+    path('image/all', views.image_block),
+    path('image/<int:image_id>', views.image_viewer),
+    path('image/<int:image_id>/add_message', views.add_message),
+    path('image/<int:image_id>/add_comment', views.add_comment),
+    path('image/<int:image_id>/delete/<int:message_id>', views.delete_message),
+
+
+####################################################################
+    path('project_detail/<int:project_id>', views.project_detail),
+    path('process_upload/<int:project_id>', views.process_upload),
+    path('edit_account/<int:user_id>', views.edit_account),
+    path('process_edit_user/<int:user_id>', views.process_edit_user),
+    path('edit_project/<int:project_id>', views.edit_project),
+    path('process_edit_project/<int:project_id>', views.process_edit_project),
+    path('process_reviewed/<int:project_id>', views.process_reviewed),
+    path('process_unreviewed/<int:project_id>', views.process_unreviewed),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
