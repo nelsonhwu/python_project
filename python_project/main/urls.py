0c205da245cd150b65641b849c9fd52109e74f43
xvang3 <69734222+xvang3@users.noreply.github.com>
2020-09-24 09:46:20 -0800

    09/24/2020
    Merge Issues: Revert

--- python_project/main/urls.py
+++ python_project/main/urls.py
@@ -4,6 +4,21 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+####################################################################
+
+    path('class/<int:class_id>/edit', views.edit_class),
+    path('class/new', views.new_class),
+    path('class/create', views.create_class),
+    path('edit_class/<int:class_id>', views.edit_class_post),
+    path('image/all', views.image_block),
+    path('image/<int:image_id>', views.image_viewer),
+    path('image/<int:image_id>/add_message', views.add_message),
+    path('image/<int:image_id>/add_comment', views.add_comment),
+    path('image/<int:image_id>/delete/<int:message_id>', views.delete_message),
+
+
+####################################################################
+
     path('', views.index),
     path('add_project/<int:class_id>', views.add_project),
     path('add_student/<int:class_id>', views.add_student),
@@ -17,5 +32,29 @@ urlpatterns = [
     path('logout', views.logout),
     path('user_info',views.user),
     path('add_relation', views.add_relation),
+<<<<<<< HEAD
+=======
+    path('index', views.index2),
 
+    path('class/<int:class_id>/edit', views.edit_class),
+    path('class/new', views.new_class),
+    path('class/create', views.create_class),
+    path('edit_class/<int:class_id>', views.edit_class_post),
+    path('image/all', views.image_block),
+    path('image/<int:image_id>', views.image_viewer),
+    path('image/<int:image_id>/add_message', views.add_message),
+    path('image/<int:image_id>/add_comment', views.add_comment),
+    path('image/<int:image_id>/delete/<int:message_id>', views.delete_message),
+
+>>>>>>> parent of 6745074... 09/24/2020
+
+####################################################################
+    path('project_detail/<int:project_id>', views.project_detail),
+    path('process_upload/<int:project_id>', views.process_upload),
+    path('edit_account/<int:user_id>', views.edit_account),
+    path('process_edit_user/<int:user_id>', views.process_edit_user),
+    path('edit_project/<int:project_id>', views.edit_project),
+    path('process_edit_project/<int:project_id>', views.process_edit_project),
+    path('process_reviewed/<int:project_id>', views.process_reviewed),
+    path('process_unreviewed/<int:project_id>', views.process_unreviewed),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
