8a5d1adcfd9e048452b4dfa65f6e71efa73b31d1
nelsonhwu <69525077+nelsonhwu@users.noreply.github.com>
2020-09-24 11:32:42 -0700

    Combining 09/24/20 11:32AM

--- python_project/main/models.py
+++ python_project/main/models.py
@@ -83,22 +83,108 @@ class UserManager(models.Manager):
             errors['subject'] = 'Subject needs at least 2 characters.'
         return errors
 
+    def edit_account_validation(self, post_data):
+        errors={}
+        if len(post_data['first_name']) != 0 and len(post_data['first_name']) < 2:
+            errors['first_name'] = 'First name needs at least 2 characters.'
+        if len(post_data['first_name']) != 0 and not NAME_REGEX.match(post_data['first_name']):
+            errors['first_name_char'] = 'First name should contain only letters'
+        
+        if len(post_data['last_name']) != 0 and len(post_data['last_name']) < 2:
+            errors['last_name'] = 'Last name needs at least 2 characters.'
+        if len(post_data['last_name']) != 0 and not NAME_REGEX.match(post_data['last_name']):
+            errors['last_name_char'] = 'Last name should contain only letters'
+        
+        if len(post_data['email']) != 0 and not EMAIL_REGEX.match(post_data['email']):
+            errors['invalid_email'] = 'Invalid email! Try Again!'
+        
+        if len(post_data['password']) != 0 and len(post_data['password']) < 8:
+            errors['password_length'] = 'Passwords need at least 8 characters.'
+        if post_data['password'] != post_data['confirm_password']:
+            errors['invalid_password'] = 'Password does not match with Confirm Password.'
+        
+        if len(post_data['street_address']) != 0 and len(post_data['street_address']) < 2:
+            errors['street_address'] = 'Your address needs at least 2 characters.'
+        if len(post_data['street_address']) != 0 and not ADDRESS_REGEX.match(post_data['street_address']):
+            errors['invalid_street_address'] =  'Please enter a valid Address.'
+        
+        if len(post_data['city']) != 0 and len(post_data['city']) < 2:
+            errors['city'] = "Please enter your city address."
+
+        if len(post_data['state']) != 0 and len(post_data['state']) < 2:
+            errors['state'] = "Please enter your state."
+
+        if len(post_data['zip_code']) != 0 and len(post_data['zip_code']) < 4:
+            errors['zip_code'] = "Please enter your 5 digit zip code."
+        
+        if len(post_data['phone']) != 0 and len(post_data['phone']) < 10:
+            errors['phone'] = 'Please enter your phone number with the area code.'
+        if len(post_data['phone']) != 0 and not PHONE_REGEX.match(post_data['phone']):
+            errors['invalid_phone'] = 'Please enter valid phone number.'
+        # if len(post_data["related_user"]) != 0 and User.objects.filter(id=post_data["related_user"]).exists() == False:
+        #     errors["related_user"] = "The parent's you put in does not exsit!"
+        return errors
+
+    def edit_project_validation(self,post_data):
+        errors={}
+        if len(post_data['title']) != 0 and len(post_data['title']) < 2:
+            errors['title'] = 'Title needs at least 2 characters.'
+        if len(post_data['due_date']) !=0 and post_data['due_date'] < date.today().strftime("%Y-%m-%d"):
+            errors['due_date'] = "Deadline cannot be in the past."
+        if len(post_data['desc']) != 0 and len(post_data['desc']) < 2:
+            errors['desc'] = 'Description needs at least 2 characters.'
+
+        return errors
+
+    def upload_image_validation(self, post_data):
+        errors ={}
+        if len(post_data['title']) < 2:
+            errors['title'] = 'Title needs at least 2 characters.'
+        return errors
+    
+    def related_person_validator(self, post_data):
+        errors={}
+        logged_in_user_email = post_data['logged_in_user_email']
+        if logged_in_user_email == post_data['email']:
+                errors['email'] = "You cannot enter your own email"
+        return errors
+
 class User(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length = 255)
     street_address = models.CharField(max_length = 255, blank=True, null=True)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     zip_code = models.IntegerField()
-    access_level = models.CharField(max_length=255)
     phone = models.IntegerField(blank=True, null=True)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length = 255)
-    # related_user = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True) recursion error
+    access_level = models.CharField(max_length = 255)
     profile_image = models.ImageField(upload_to='profile_image', blank=True, null=True, default="profile1.png")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
+    relationships = models.ManyToManyField('self', through='Relationship', symmetrical=False, related_name='related_to')
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
+    
+RELATIONSHIP_PARENT = 1
+RELATIONSHIP_GUARDIAN = 2
+RELATIONSHIP_STUDENT = 3
+RELATIONSHIP_SIBLING = 4
+RELATIONSHIP_STATUSES = (
+    (RELATIONSHIP_PARENT, 'Parent'),
+    (RELATIONSHIP_GUARDIAN, 'Guardian'),
+    (RELATIONSHIP_STUDENT, 'Student'),
+    (RELATIONSHIP_SIBLING, 'Sibling'),
+)
+
+class Relationship(models.Model):
+    from_user = models.ForeignKey(User, related_name='from_users', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='to_users', on_delete=models.CASCADE)
+    status = models.IntegerField(choices=RELATIONSHIP_STATUSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Class(models.Model):
     # teacher = models.ManyToManyField(Teacher, related_name="teacher_classes")
--- python_project/main/views.py
+++ python_project/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.contrib import messages
-from .models import User, Class, Project, Message, Comment, Image
+from .models import User, Class, Project, Message, Comment, Image, Relationship
 import bcrypt
+from datetime import date, datetime, timezone
+from django.contrib import messages
+
+
+##########################################################################################################
 
 def index(request):
     return render(request, "homepage.html")
@@ -12,11 +16,12 @@ def add_project(request, class_id):
     due_date = request.POST['due_date']
     desc = request.POST['desc']
     new_project = Project.objects.create(
+        course = current_class,
         title = title, 
         due_date = due_date, 
         desc = desc
     )
-    current_class.project_classes.add(new_project)
+    # current_class.project_classes.add(new_project)
     return redirect(f'/class/{class_id}')
 
 def add_student(request, class_id):
@@ -153,9 +158,11 @@ def success(request):
     if 'user_id' not in request.session:
         return redirect('/')
     logged_in_user = User.objects.get(id=request.session['user_id'])
-    print(logged_in_user.__dict__)
+
+    all_relationships = Relationship.objects.all()
     context = {
         'logged_in_user' : logged_in_user,
+        'all_relationships' : all_relationships,
     }
     return render(request, "success.html", context)
 
@@ -174,7 +181,7 @@ def image_block(request):
     context = {
         'all_images': Image.objects.all()
     }
-    return render(request, 'image.html', context)
+    return render(request, 'images.html', context)
 
 def image_viewer(request, image_id):
     current_image = Image.objects.get(id=image_id)
@@ -203,3 +210,276 @@ def delete_message(request, image_id, message_id):
     message = Message.objects.get(id=message_id)
     message.delete()
     return redirect(f'/image/{image_id}')
+
+def register(request):
+    return render(request, "register.html")
+
+def add_user(request):
+    errors = User.objects.registration_validator(request.POST)
+    if len(errors) > 0:
+        for msg in errors.values():
+            messages.error(request, msg)
+        return redirect('/register')
+    password = request.POST['password']
+    pw_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
+    new_user = User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        password = pw_hash,
+        street_address = request.POST['street_address'],
+        city = request.POST['city'],
+        state = request.POST['state'],
+        zip_code = request.POST['zip_code'],
+        phone = request.POST['phone'],
+        access_level = request.POST['access_level']
+    )
+    request.session['user_id'] = new_user.id
+    return redirect('/success')
+
+def login(request):
+    return render(request, "login.html")
+
+def log_in(request):
+    errors = User.objects.login_validation(request.POST)
+    if len(errors) > 0:
+        for msg in errors.values():
+            messages.error(request, msg)
+        return redirect('/login')
+    list_of_users = User.objects.filter(email=request.POST['email'])
+    if len(list_of_users) > 0:
+        user = list_of_users[0]
+        if bcrypt.checkpw(request.POST['password'].encode('utf-8'), user.password.encode()):
+            request.session['user_id'] = user.id
+            return redirect('/user_info')
+        return redirect('/')
+
+def success(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    logged_in_user = User.objects.get(id=request.session['user_id'])
+    print(logged_in_user.__dict__)
+    context = {
+        'logged_in_user' : logged_in_user,
+    }
+    return render(request, "success.html", context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def user(request):
+    logged_in_user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'logged_in_user':logged_in_user,
+    }
+    return render(request, 'user_info.html',context)
+
+def add_relation(request):
+    if 'user_id' not in request.session:
+        return('/')
+    logged_in_user = User.objects.get(id=request.session['user_id'])
+    errors = User.objects.related_person_validator(request.POST)
+    if len(errors) > 0:
+        for msg in errors.values():
+            messages.error(request, msg)
+        return redirect("/success")        
+    list_of_users = User.objects.filter(email=request.POST['email'])
+    print("Working Here1")
+    if len(list_of_users) > 0:
+        person_to_add = list_of_users[0]
+        print("%"*60)
+        print(person_to_add)
+        Relationship.objects.create(
+            from_user=logged_in_user,
+            to_user=person_to_add,
+            status=request.POST['status']
+        )
+        return redirect("/success")
+    else:
+        return redirect("/success")
+
+
+
+
+
+
+##########################################################################################################
+
+def edit_account(request, user_id):
+    if "user_id" in request.session:
+
+        logged_user = User.objects.get(id=user_id)
+
+        return render(request, "accountEdit.html", {"logged_user": logged_user})
+    return redirect("/")
+
+def update (post_field, user_id):
+    # logged_user = User.objects.get(id=user_id)
+    # if post_field == "profile_image":
+    #     profile_image = request.FILES["profile_image"]
+    #     logged_user.profile_image.save(profile_image.name, profile_image)
+    #     logged_user.save()
+    #     return logged_user
+    if len(request.POST[post_field]) !=0 and post_field != "password":
+        logged_user[post_field] = request.POST[post_field]
+        logged_user.save()
+        return logged_user
+
+    elif len(request.POST[post_field]) !=0 and post_field == "password":
+        pw = request.POST[post_field]
+        pw_hash = bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt()).decode()
+        logged_user[post_field] = pw_hash
+        logged_user.save()
+        return logged_user
+
+
+def process_edit_user(request, user_id):
+        user_id = user_id
+        logged_user = User.objects.get(id=user_id)
+        errors = User.objects.edit_account_validation(request.POST)
+
+        if len(errors) > 0:
+            for value in errors.values():
+                messages.error(request, value, extra_tags="edit_err")
+            return redirect(f"/edit_account/{user_id}")
+        else:
+            if "profile_image" in request.FILES:
+                profile_image = request.FILES["profile_image"]
+                logged_user.profile_image.save(profile_image.name, profile_image)
+                logged_user.save()
+            if len(request.POST["password"]) !=0:
+                pw = request.POST[post_field]
+                pw_hash = bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt()).decode()
+                logged_user.password = pw_hash
+                logged_user.save()
+            if len(request.POST["first_name"]) !=0:
+                logged_user.first_name = request.POST["first_name"]
+                logged_user.save()
+            if len(request.POST["last_name"]) !=0:
+                logged_user.last_name = request.POST["last_name"]
+                logged_user.save()
+            if len(request.POST["email"]) !=0:
+                logged_user.email = request.POST["email"]
+                logged_user.save()
+            if len(request.POST["street_address"]) !=0:
+                logged_user.street_address = request.POST["street_address"]
+                logged_user.save()
+            if len(request.POST["city"]) !=0:
+                logged_user.city = request.POST["city"]
+                logged_user.save()
+            if len(request.POST["state"]) !=0:
+                logged_user.state = request.POST["state"]
+                logged_user.save()
+            if len(request.POST["zip_code"]) !=0:
+                logged_user.zip_code = request.POST["zip_code"]
+                logged_user.save()
+            if len(request.POST["phone"]) !=0:
+                logged_user.phone = request.POST["phone"]
+                logged_user.save()
+            success_message = "Profile updated successfully!"
+            messages.success(request, success_message)
+            return redirect(f"/edit_account/{logged_user.id}")
+
+
+
+            success_message = "Profile updated successfully!"
+            messages.success(request, success_message)
+            return redirect(f"/edit_account/{user_id}")
+
+def project_detail(request, project_id):
+    if "user_id" in request.session:
+
+        logged_user = User.objects.get(id=request.session["user_id"])
+        cur_proj = Project.objects.get(id=project_id)
+        total_images = len(cur_proj.images.all())
+
+        context = {
+            "logged_user": logged_user,
+            "cur_proj": cur_proj,
+            "total_images": total_images,
+        }
+
+        return render(request, "projectDetail.html", context)
+    return redirect("/")
+
+def process_upload(request, project_id):
+    logged_user = User.objects.get(id=request.session["user_id"])
+    cur_proj = Project.objects.get(id=project_id)
+
+    errors = Project.objects.upload_image_validation(request.POST)
+    if len(request.FILES["picture"]) ==0:
+        errors["picture"] = "You must include you work to submit!"
+    if len(errors) > 0:
+            for value in errors.values():
+                messages.error(request, value, extra_tags="upload_img_err")
+            return redirect(f"/process_upload/{cur_proj.id}")
+    else:
+        title = request.POST["title"]
+        image = request.FILES["picture"]
+        new_image = Image()
+        new_image.title = title
+        new_image.submission = cur_proj
+        new_image.submitter = logged_user
+        new_image.save()
+        new_image.picture.save(image.name, image)
+
+        return redirect("/image/all")
+
+
+def process_reviewed(request, project_id):
+    logged_user = User.objects.get(id=request.session["user_id"])
+    cur_proj = Project.objects.get(id=project_id)
+    cur_proj.reviewed_by_user.add(logged_user)
+    cur_proj.save()
+    return redirect(f'/project_detail/{cur_proj.id}')
+
+def process_unreviewed(request, project_id):
+    logged_user = User.objects.get(id=request.session["user_id"])
+    cur_proj = Project.objects.get(id=project_id)
+    cur_proj.reviewed_by_user.remove(logged_user)
+    cur_proj.save()
+    return redirect(f'/project_detail/{cur_proj.id}')
+
+
+def edit_project(request, project_id):
+    if "user_id" in request.session:
+
+        logged_user = User.objects.get(id=request.session["user_id"])
+        cur_proj = Project.objects.get(id=project_id)
+        all_classes = Class.objects.all()
+        context = {
+            "logged_user": logged_user,
+            "cur_proj": cur_proj,
+            "all_classes": all_classes,
+        }
+        return render(request, "projectEdit.html", context)
+    return redirect("/")
+
+
+def process_edit_project(request, project_id):
+    logged_user = User.objects.get(id=request.session["user_id"])
+    cur_proj = Project.objects.get(id=project_id)
+    errors = Project.objects.edit_project_validation(request.POST)
+    if len(errors) > 0:
+        for value in errors.values():
+            messages.error(request, value, extra_tags="edit_proj_err")
+        return redirect(f"/edit_project/{cur_proj.id}")
+    else:
+        if len(request.POST["title"]) !=0:
+            cur_proj.title = request.POST["title"]
+            # cur_proj.save()
+        if len(request.POST["due_date"]) !=0:
+            cur_proj.due_date = request.POST["due_date"]
+            # cur_proj.save()
+        if len(request.POST["desc"]) !=0:
+            cur_proj.desc = request.POST["desc"]
+        if len(request.POST["course"]) !=0:
+            course = Class.objects.get(id=request.POST["course"])
+            cur_proj.course = course
+        cur_proj.save()
+        success_message = "Project updated successfully!"
+        messages.success(request, success_message)
+        return redirect(f"/edit_project/{cur_proj.id}")
+        
+
